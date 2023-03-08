f467fda8fd0e70df48a613f9c04a75d12b2616ad
Geng Sun <gsun@iastate.edu>
2023-03-08 15:36:40 -0600

    done

--- Controllers/BankOZKController.cs
+++ Controllers/BankOZKController.cs
@@ -5,12 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/[controller]")]
 public class ProductV1Controller : ControllerBase
 {
-
-
-    [HttpGet]
-    public ActionResult GetAllUsers()
-    {
-        Bankuser[] bankUsers = {
+    private Bankuser[] bankUsers = {
             new Bankuser { Id = 1,
                     Firstname = "John",
                     Lastname = "Doe",
@@ -33,28 +28,60 @@ public class ProductV1Controller : ControllerBase
                     CreatedAt = DateTime.Now}
             };
 
-        Bankaccount[] bankAccounts = {
+    private Bankaccount[] bankAccounts = {
             new Bankaccount { Id = 1,
                     AccountType = (int?)AccountType.Checkings,
                     Amount = 100,
-                    BankUserId = 2,
+                    BankUserId = 1,
+                    Enabled = 1,
                     CreatedAt = DateTime.Now},
             new Bankaccount { Id = 2,
                     AccountType = (int?)AccountType.Savings,
                     Amount = 2000,
                     BankUserId = 1,
+                    Enabled = 1,
                     CreatedAt = DateTime.Now},
             new Bankaccount { Id = 3,
                     AccountType = (int?)AccountType.MoneyMarket,
                     Amount = 3000,
                     BankUserId = 3,
+                    Enabled = 1,
                     CreatedAt = DateTime.Now}
             };
 
+    [HttpGet]
+    public ActionResult GetAllUsers()
+    {
         foreach (Bankuser user in bankUsers)
         {
             user.Bankaccounts = bankAccounts.Where(ba => ba.BankUserId == user.Id).ToList();
         }
         return Ok(bankUsers.ToArray());
     }
+
+    [HttpPost]
+    public ActionResult MoneyTransfer([FromBody] MoneyTransferParam moneyTransferParam)
+    {
+        Bankaccount sourceAccount = bankAccounts.Where(ba => ba.Id == moneyTransferParam.SourceAccountId).FirstOrDefault();
+        Bankaccount destinationAccount = bankAccounts.Where(ba => ba.Id == moneyTransferParam.DestAccountId).FirstOrDefault();
+
+
+        if (sourceAccount.Amount < moneyTransferParam.TransferAmount
+            || sourceAccount.Enabled == 0
+            || destinationAccount.Enabled == 0)
+        {
+            sourceAccount.Enabled = 0;
+            return BadRequest("Insufficient funds");
+        }
+
+        sourceAccount.Amount -= moneyTransferParam.TransferAmount;
+        destinationAccount.Amount += moneyTransferParam.TransferAmount;
+
+        Bankuser user = bankUsers.Where(bu => bu.Id == moneyTransferParam.BankUserId).FirstOrDefault();
+        user.Bankaccounts = bankAccounts.Where(ba => ba.BankUserId == user.Id).ToList();
+
+        return Ok(user);
+    }
+
+
 }
